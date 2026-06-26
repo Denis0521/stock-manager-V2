336c91bb84a0475f3a93fd96e7bd2d99db20e57e
Denis0521 <87504630+Denis0521@users.noreply.github.com>
2026-06-26 15:19:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'stock-portfolio-v4.14.0';
+const CACHE_NAME = 'stock-portfolio-v4.14.2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png', // 已補上 192x192 圖示，確保符合 Chrome 的安裝標準
+  './icon-192.png',
   './icon-512.png'
 ];
 
