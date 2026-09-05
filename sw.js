622b4cc200136ce5ece9b934e1ba67f17cddd39a
Denis0521 <87504630+Denis0521@users.noreply.github.com>
2026-09-05 10:48:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-app-V7.38'; // 同步更新 Service Worker 快取版本
+const CACHE_NAME = 'stock-app-V7.40'; // 同步更新 Service Worker 快取版本
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
